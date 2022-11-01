ee9db08d8d7fb519c103aa6d645b64fd3b570f5d
wjahoward <howardwang42@gmail.com>
2022-11-01 16:44:21 +0800

    created partner entity

---
+++ CarRentalManagementLibrary/src/entity/Partner.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author wjahoward
+ */
+@Entity
+public class Partner implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long partnerId;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    public Partner() {
+        
+    }
+    
+    public Partner(String name) {
+        this.name = name;
+    }
+
+    public Long getPartnerId() {
+        return partnerId;
+    }
+
+    public void setPartnerId(Long partnerId) {
+        this.partnerId = partnerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (partnerId != null ? partnerId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the partnerId fields are not set
+        if (!(object instanceof Partner)) {
+            return false;
+        }
+        Partner other = (Partner) object;
+        if ((this.partnerId == null && other.partnerId != null) || (this.partnerId != null && !this.partnerId.equals(other.partnerId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Partner[ id=" + partnerId + " ]";
+    }
+    
+}
